10f67640e625f06c6f99f9bb738d40bef9cf1c97
Lucianogs90 <lucianogs90@gmail.com>
2022-10-05 18:23:35 -0400

    criado o método de addAmigo

--- aula_04_out_2022/RedeSocial.java
+++ aula_04_out_2022/RedeSocial.java
@@ -5,12 +5,12 @@ public class RedeSocial {
     public class Usuario{
         public int id;
         public String nome;
-        public Usuario amigos[][] = new Usuario[1][1];
+        public Usuario amigos[][] = new Usuario[][];
         
         public Usuario(int id, String nome){
             this.id = id;
             this.nome = nome;
-            this.amigos[id][0] = null;
+            this.amigos[this.id][0] = this;
         }
 
     }
@@ -22,11 +22,14 @@ public class RedeSocial {
         Usuario novosAmigos[][] = new Usuario[this.id][this.amigos.length + 1];
         
         for (int i = 0; i < this.amigos.length; i++){
-            novosAmigos[i] = this.amigos[i];
+            novosAmigos[this.id][i] = this.amigos[this.id][i];
         }
 
-        novosAmigos[this.amigos.length] = novoAmigo;
+        novosAmigos[this.id][this.amigos.length] = novoAmigo;
     }
 
+    public Usuario[] recomendarAmigos(int max){
+        
+    }
 
 }
